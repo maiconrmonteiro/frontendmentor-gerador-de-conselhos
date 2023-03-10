75cc28f6c4ee13b519858df8b1ebf220de82246a
maiconrmonteiro <maiconrmonteiro@gmail.com>
2023-03-10 14:07:56 -0300

    pequenas correções

--- src/js/gerador-conselhos.js
+++ src/js/gerador-conselhos.js
@@ -10,13 +10,12 @@ async function atualizarConselho(){
     const data = await resposta.json()
     const conselho = data.slip.advice
     const id = data.slip.id
-    console.log(conselho)
 
     document.getElementById('id').innerText = `# ${id}`
-    document.getElementById('conselho').innerHTML = conselho
-
-
+    document.getElementById('conselho').innerHTML = `“${conselho}”`
 
 }
 
 atualizarConselho()
+
+
